c20e3a5f1b9b82ce22cd39dca435b87d7e688200
matsete <csd2571@csd.uoc.gr>
2015-03-24 16:46:41 +0200

    poulaw ton giwrgadaki 3 eurw

---
+++ Syntax_Analyzer/symtable.h
@@ -0,0 +1,31 @@
+ 
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#ifndef _SYM_TABLE_H_
+#define _SYM_TABLE_H_
+
+/* my symbol table structure */
+typedef struct table *SymTable_T;
+
+/* returns a new empty item of type SymTable_T */
+SymTable_T SymTable_new();
+
+/* frees all memory used by oSymtable */
+void SymTable_free(SymTable_T oSymTable);
+
+/* inserts a new binding in oSymTable. Returns True on success and False on failure */
+int Insert(SymTable_T oSymTable, const char *symbol, const char *name, const void *type, int scope, const char* formal_args, int line);
+
+/* removes the binding of given pcKey. Returns True on success and False on failure */
+int Hide(SymTable_T oSymTable, const char *symbol, const char *name, int line);
+
+/* Returns True if binding with given pcKey is found or False on failure */
+int Lookup(SymTable_T oSymTable, const char *symbol, const char *name, int line);
+
+
+#endif
