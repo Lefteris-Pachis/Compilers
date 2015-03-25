4a39c76a61979851b112add78faac959c64b0bf1
matsete <csd2571@csd.uoc.gr>
2015-03-25 14:19:23 +0200

    peos

--- Syntax_Analyzer/symtable.h
+++ Syntax_Analyzer/symtable.h
@@ -9,23 +9,29 @@
 #ifndef _SYM_TABLE_H_
 #define _SYM_TABLE_H_
 
-struct Node{
+#define BUCKETS 100
+
+
+typedef struct Node{
 	char* var_name;
 	char* var_type;
-	int var_scope;
-	int var_line;
 
 	char* func_name;
 	char* func_type;
 	char* func_args;
-	int func_scope;
-	int func_line;
+
+	int scope;
+	int line;
+	int hiden;
 
 	struct Node *next;
-};
+}*node_t;
 
+typedef struct table{
+
+	node_t hashtable[BUCKETS];
+}*SymTable_T;
 /* my symbol table structure */
-typedef struct table *SymTable_T;
 
 /* returns a new empty item of type SymTable_T */
 SymTable_T SymTable_new();
@@ -39,10 +45,13 @@ int Insert_Var(SymTable_T oSymTable, const char *var_name, const char *var_type,
 int Insert_Func(SymTable_T oSymTable, const char *func_name, const char *func_type, const void *func_args, int func_scope, int func_line);
 
 /* removes the binding of given pcKey. Returns True on success and False on failure */
-int Hide(SymTable_T oSymTable, const char *name, int line);
+int Hide(SymTable_T oSymTable, const char *name, int line, int hiden);
 
 /* Returns True if binding with given pcKey is found or False on failure */
-int Lookup(SymTable_T oSymTable, const char *name, int line);
+int Lookup(SymTable_T oSymTable, const char *name, int line, int scope);
 
 
-#endifs
+void Print_Hash(SymTable_T oSymTable);
+
+
+#endif
